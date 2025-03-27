f42e6584cc2c42100c2dbd67fedb5d8f06e5e6e9
nicoramo2s <nicolasramo2s@hotmail.com>
2025-03-27 00:19:01 -0300

    add Stripe configuration to environment variables and update payment service URLs

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,6 +4,9 @@ import 'dotenv/config';
 interface EnvVars {
   PORT: number;
   STRIPE_SECRET_KEY: string;
+  STRIPE_SUCCESS_URL: string;
+  STRIPE_CANCEL_URL: string;
+  STRIPE_ENDPOINT_SECRET: string;
 }
 
 // Validamos las variables de entorno
@@ -11,6 +14,9 @@ const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     STRIPE_SECRET_KEY: joi.string().required(),
+    STRIPE_SUCCESS_URL: joi.string().required(),
+    STRIPE_CANCEL_URL: joi.string().required(),
+    STRIPE_ENDPOINT_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -25,4 +31,7 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   stripeSecretKey: envVars.STRIPE_SECRET_KEY,
+  stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
+  stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
+  stripeEndpointSecret: envVars.STRIPE_ENDPOINT_SECRET,
 };
