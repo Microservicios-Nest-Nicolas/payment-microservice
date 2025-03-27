0d9fa9c88e5f10bfa7ddc50e471f6fc6c914378e
nicoramo2s <nicolasramo2s@hotmail.com>
2025-03-26 23:50:01 -0300

    initialize payments microservice with basic configuration and structure

---
+++ src/config/envs.ts
@@ -0,0 +1,28 @@
+import * as joi from 'joi';
+import 'dotenv/config';
+
+interface EnvVars {
+  PORT: number;
+  STRIPE_SECRET_KEY: string;
+}
+
+// Validamos las variables de entorno
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    STRIPE_SECRET_KEY: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  stripeSecretKey: envVars.STRIPE_SECRET_KEY,
+};
---
+++ src/payments/payments.service.ts
@@ -0,0 +1,80 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment.-session.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.stripeSecretKey);
+
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: { name: item.name },
+          unit_amount: Math.round(item.price * 100),
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const session = await this.stripe.checkout.sessions.create({
+      // Colocar el id de la orden
+      payment_intent_data: {
+        metadata: {
+          orderId: orderId,
+        },
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success',
+      cancel_url: 'http://localhost:3003/payments/cancel',
+    });
+
+    return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+    // Testing
+    // const endpointSecret =
+    //   'whsec_91464959565bf8d2dbf276571be853864da5378e07a45e0c35161f1ba2ae59e8';
+
+    const endpointSecret = 'whsec_oIZL67YmjjZSOALPmwdMJvGSbFJPDVJY';
+
+    let event: Stripe.Event;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig || '',
+        endpointSecret,
+      );
+    } catch (error) {
+      res
+        .status(HttpStatus.BAD_REQUEST)
+        .send(`Webhook Error: ${error.message}`);
+      return;
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceeded = event.data.object;
+        console.log({
+          metadata: chargeSucceeded.metadata,
+          orderId: chargeSucceeded.metadata.orderId,
+        });
+        break;
+
+      default:
+        console.log(`Event ${event.type} not handled`);
+        break;
+    }
+
+    return res.status(HttpStatus.OK).json({ sig });
+  }
+}
